bd395ce853025f325fcd13d0f93bc9b4581392b5
Anubhav Tiwari <133010223+Anubhavt07@users.noreply.github.com>
2024-08-08 22:10:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('.login-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Login button clicked!');
+});
+
+document.querySelector('.create-account').addEventListener('click', function() {
+  alert('Create Account button clicked!');
+});
