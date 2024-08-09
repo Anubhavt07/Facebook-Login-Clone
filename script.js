7d034303233fe6a0899ed7b38240f67c573f21aa
Anubhav Tiwari <133010223+Anubhavt07@users.noreply.github.com>
2024-08-09 08:55:26 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.login-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Login button clicked!');
-});
-
-document.querySelector('.create-account').addEventListener('click', function() {
-  alert('Create Account button clicked!');
-});
